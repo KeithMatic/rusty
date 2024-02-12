57209bbcb0ea8d10afaa557bb9d7e6500de0b361
Keith Mngadi <38478562+KeithMatic@users.noreply.github.com>
2024-02-12 11:26:23 +0200

    update

--- conditionals/src/main.rs
+++ conditionals/src/main.rs
@@ -1,4 +1,21 @@
 fn main() {
-    let floaty = 24.5_f64.round();
-    println!("{floaty}")
+    let number = 6;
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+    // Because "if" is an expression, we can use it on the RHS of a "let" statement to assign the outcome to a variable, as in Listing 3-2.
+
+    let condition = true;
+
+    let number_one = if condition { 5 } else { 6 };
+    println!("{number_one}");
+
+    // let condition = gameOver ? "You lose!" : "Continue playing!"; JavaScript
 }
---
+++ loops/target/debug/loops.d
@@ -0,0 +1,1 @@
+/Users/matic/Developer/rust/rusty/loops/target/debug/loops: /Users/matic/Developer/rust/rusty/loops/src/main.rs
